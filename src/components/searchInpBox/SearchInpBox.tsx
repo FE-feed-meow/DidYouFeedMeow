85372a2fa2fca89a378dc8710440653a830dcdf8
Nam-Wijeong <dnlwjdv@hanmail.net>
2022-10-10 22:32:50 +0900

    :sparkles: Feat : 장소,주소 검색페이지 마크업 구현

--- src/components/searchInpBox/SearchInpBox.tsx
+++ src/components/searchInpBox/SearchInpBox.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import Image from '../../atoms/image/Image';
 
 const SearchInpBox = () => {
   return (
-    <div>SearchInpBox</div>
+    <>
+      <SearchWrap>
+        <Image 
+          width={20}
+          src="assets/icons/icon-menu.svg" 
+          alt="메뉴 버튼" />
+          <SearchInput type="text" placeholder='장소, 주소를 입력해주세요' />
+      </SearchWrap>
+    </>
   )
 }
 
 export default SearchInpBox
+
+const SearchWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 48px;
+  padding: 0 30px;
+  background-color: var(--background-color);
+  box-shadow: rgba(0, 0, 0, 0.1) 0 4px 4px 0;
+`
+
+const SearchInput = styled.input`
+  width: 307px;
+  background-color: #f9f9f9;
+  margin-left: 12px;
+  padding: 4px 32px;
+  font-size: 14px;
+`
--- src/pages/recordMapPage/RecordMapPage.tsx
+++ src/pages/recordMapPage/RecordMapPage.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
+import SearchInpBox from '../../components/searchInpBox/SearchInpBox'
 
 const RecordMapPage = () => {
   return (
-    <div>RecordMapPage</div>
+    <div style={{position:'relative'}} >
+      <SearchInpBox/>
+      <PlusBtn type="button">
+        <img src="assets/icons/icon-plus.svg" alt="플러스 버튼" />
+      </PlusBtn>
+    </div>
   )
 }
 
 export default RecordMapPage
+
+const PlusBtn = styled.button`
+  width: 60px;
+  height: 60px;
+  position: absolute;
+  top: 723px;
+  right: 40px;
+`
+
