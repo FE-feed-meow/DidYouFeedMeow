a155fce846365bbfa5d0746164118467d6601abb
leeyeun <dpdms0109@naver.com>
2022-10-11 17:06:23 +0900

    :sparkles: Feat : 취소 버튼 생성

---
+++ src/atoms/button/closeBtn/CloseBtn.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { CloseBWrap, ClBnt } from "./style";
+
+const CloseBtn = () => {
+  return (
+    <CloseBWrap>
+      <ClBnt />
+    </CloseBWrap>
+  );
+};
+
+export default CloseBtn;
---
+++ src/atoms/button/closeBtn/style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const CloseBWrap = styled.div`
+  display: inline-block;
+  margin-left: auto;
+`;
+
+const ClBnt = styled.button`
+  width: 23px;
+  height: 23px;
+  background: url("../../../assets/icons/icon-close.svg");
+`;
+
+export { CloseBWrap, ClBnt };
