b61e5d21ab8fbdd33c986089ef3baf0a4d47fb8c
Hyebin-Woo <dev.hyebin@gmail.com>
2022-10-12 18:11:24 +0900

    :sparkles: Feat : 냥이 식량 추가되는 컴포넌트 구현

---
+++ src/components/catFeed/feedCard/FeedCard.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import styled from "styled-components";
+
+import Icon from "../../../atoms/icon/Icon";
+
+export const CardWrap = styled.section`
+  position: relative;
+  width: 290px;
+  margin-bottom: 26px;
+  display: flex;
+  align-items: flex-end;
+`;
+
+export const FeedTime = styled.h2`
+  font-weight: 700;
+  font-size: 16px;
+  padding-bottom: 15px;
+`;
+
+export const Feedinfo = styled.div`
+  margin-left: 23px;
+`;
+
+export const FeedName = styled.h3`
+  font-weight: 500;
+  font-size: 14px;
+  padding-bottom: 5px;
+`;
+
+export const FeedEtc = styled.p`
+  font-size: 12px;
+  color: #797979;
+`;
+
+export const IconBox = styled.div`
+  position: absolute;
+  top: -3px;
+  right: -25px;
+`;
+
+const FeedCard = () => {
+  return (
+    <CardWrap>
+      <FeedTime>20:00</FeedTime>
+      <Feedinfo>
+        <FeedName>간식(츄르)</FeedName>
+        <FeedEtc>몸이 안 좋은지 밥을 잘 먹지 못하네요..</FeedEtc>
+      </Feedinfo>
+      <IconBox>
+        <button type="button">
+          <Icon
+            width={19}
+            height={19}
+            alt=""
+            src="assets/icons/icon-delete.svg"
+          />
+        </button>
+      </IconBox>
+    </CardWrap>
+  );
+};
+
+export default FeedCard;
