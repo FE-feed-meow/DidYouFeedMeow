7971b1e705684c5a493a4614e11edc73e0943101
Hyebin-Woo <dev.hyebin@gmail.com>
2022-10-12 18:12:52 +0900

    :sparkles: 냥이 정보 상세 페이지 마크업 구현

--- src/pages/catInfoPage/CatInfoPage.tsx
+++ src/pages/catInfoPage/CatInfoPage.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+
+import Header from "../../components/header/Header";
+import Button from "../../atoms/button/Button";
+
+import CatInfo from "../../components/catInfo/CatInfo";
+import CatFeed from "../../components/catFeed/CatFeed";
+
+export const Wrap = styled.div`
+  padding: 15px 40px 37px;
+`;
 
 const CatInfoPage = () => {
   return (
-    <div>CatInfoPage</div>
-  )
-}
+    <>
+      <Header />
+      <Wrap>
+        <CatInfo />
+        <CatFeed />
+        <Button marginTop={10} bgColor="var(--main-color)">
+          밥 후기
+        </Button>
+      </Wrap>
+    </>
+  );
+};
 
-export default CatInfoPage
+export default CatInfoPage;
