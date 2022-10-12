046deb5e9729236f2a791365ae062d3cc5729d39
leeyeun <dpdms0109@naver.com>
2022-10-12 18:50:08 +0900

    :sparkles: Feat : 고양이 상세페이지 수정,삭제 모달 생성

---
+++ src/components/modal/catModal/CatModal.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  ModalMain,
+  ModalArea,
+  ModalWrap,
+  CatModalWrap,
+  CatInfoModify,
+  CatInfoDelete,
+} from "./style";
+import { CloseBtn } from "../catInfoModal/style";
+
+const CatModal = () => {
+  return (
+    <ModalMain>
+      <ModalArea>
+        <CloseBtn />
+        <ModalWrap>
+          <CatModalWrap>
+            <CatInfoModify>냥 정보 수정</CatInfoModify>
+            <CatInfoDelete>냥 정보 삭제</CatInfoDelete>
+          </CatModalWrap>
+          <CatModal />
+        </ModalWrap>
+      </ModalArea>
+    </ModalMain>
+  );
+};
+
+export default CatModal;
