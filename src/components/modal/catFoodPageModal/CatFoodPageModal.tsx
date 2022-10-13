ffed18d987aa19ace7234bf3567b8eb860d53157
leeyeun <dpdms0109@naver.com>
2022-10-14 02:01:14 +0900

    :sparkles: Feat : 밥주기 모달 마크업 생

---
+++ src/components/modal/catFoodPageModal/CatFoodPageModal.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import CloseBtn from "../../../atoms/button/closeBtn/CloseBtn";
+import { ModalArea, ModalMain, ModalWrap } from "../style";
+
+import {
+  CatFoodWrap,
+  CatFoodTitle,
+  CatFoodTimeSelect,
+  CatFoodButtonArea,
+  CatFoodButton,
+  CatFoodButtonWrap,
+  CatFeedButton,
+  CatFeedImage,
+  CatFoodInput,
+  CatFoodSubmitButton,
+} from "./style";
+
+const CatFoodPageModal = () => {
+  return (
+    <ModalMain>
+      <ModalArea>
+        <CloseBtn />
+        <ModalWrap>
+          <CatFoodWrap>
+            <CatFoodTitle>시간</CatFoodTitle>
+            <CatFoodTimeSelect>
+              <option value="시">시</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+            </CatFoodTimeSelect>
+            <CatFoodTimeSelect>
+              <option value="분">분</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+            </CatFoodTimeSelect>
+            <CatFoodTitle>어떤 것을 주셨나요?</CatFoodTitle>
+            <CatFoodButtonArea>
+              <CatFoodButtonWrap>
+                <CatFeedImage src="/assets/icons/icon-feed.svg" alt="" />
+                <CatFeedButton>밥</CatFeedButton>
+              </CatFoodButtonWrap>
+              <CatFoodButtonWrap>
+                <CatFeedImage src="/assets/icons/icon-water.svg" alt="" />
+                <CatFoodButton>물</CatFoodButton>
+              </CatFoodButtonWrap>
+              <CatFoodButtonWrap>
+                <CatFeedImage src="/assets/icons/icon-treat.svg" alt="" />
+                <CatFoodButton>간식</CatFoodButton>
+              </CatFoodButtonWrap>
+            </CatFoodButtonArea>
+            <CatFoodInput placeholder="2-15자 이내여야 합니다." />
+            <CatFoodTitle>특이사항</CatFoodTitle>
+            <CatFoodInput placeholder="2-15자 이내여야 합니다." />
+            <CatFoodSubmitButton>저장하기</CatFoodSubmitButton>
+          </CatFoodWrap>
+        </ModalWrap>
+      </ModalArea>
+    </ModalMain>
+  );
+};
+
+export default CatFoodPageModal;
--- src/components/modal/style.ts
+++ src/components/modal/style.ts
@@ -1,1 +1,29 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+
+export const ModalMain = styled.div`
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(55, 55, 55, 0.5);
+  z-index: 100;
+`;
+
+export const ModalArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+export const ModalWrap = styled.div`
+  box-sizing: border-box;
+  width: 300px;
+  border-radius: 55px;
+  overflow: hidden;
+  background-color: var(--background-color);
+`;
