beefd21ee74eb9b12c48b86bf4d822e7f46316bf
hee1231 <darong018@naver.com>
2022-10-14 12:10:48 +0900

    :sparkles: Feat: 404 페이지 마크업 구현

---
+++ src/pages/errorPage/ErrorPage.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components";
+import Image from "../../atoms/image/Image";
+
+const Wrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  p {
+    font-size: 20px;
+    font-family: "Shinb7Regular";
+  }
+`;
+
+const ErrorButton = styled.button`
+  width: 200px;
+  height: 44px;
+  margin-top: 30px;
+  font-size: 16px;
+  border-radius: 25px;
+  color: #ffffff;
+  background-color: var(--main-color);
+`;
+
+const ErrorPage = () => {
+  return (
+    <Wrap>
+      <Image
+        marginTop={328}
+        marginBottom={27}
+        width={103}
+        height={73}
+        alt=""
+        src="assets/icons/icon-error.svg"
+      />
+      <p>페이지를 찾을 수 없습니다. :(</p>
+      <ErrorButton>이전 페이지</ErrorButton>
+    </Wrap>
+  );
+};
+
+export default ErrorPage;
