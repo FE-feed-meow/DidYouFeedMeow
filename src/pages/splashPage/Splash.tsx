cdf889153c484e7f73e22da8ac91cb1308a19c64
hee1231 <darong018@naver.com>
2022-10-14 11:41:37 +0900

    :sparkles: Feat: 스플래시 페이지 마크업 구현

--- src/pages/splashPage/Splash.tsx
+++ src/pages/splashPage/Splash.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import Image from "../../atoms/image/Image";
+
+const Wrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  p {
+    font-size: 48px;
+    font-family: "Shinb7Regular";
+  }
+`;
 
 const Splash = () => {
   return (
-    <div>Splash</div>
-  )
-}
+    <Wrap>
+      <Image
+        marginTop={300}
+        marginBottom={15}
+        width={113}
+        height={85}
+        alt=""
+        src="assets/images/logo.svg"
+      />
+      <p>밥줬냥</p>
+    </Wrap>
+  );
+};
 
-export default Splash
+export default Splash;
