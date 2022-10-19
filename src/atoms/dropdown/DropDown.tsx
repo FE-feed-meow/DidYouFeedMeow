872b19f739ff8a4f185ea7814e03907ff4f128db
Hyebin-Woo <dev.hyebin@gmail.com>
2022-10-19 23:14:55 +0900

    :lipstick: Modify: width props 스타일컴포넌트 타입 지정

---
+++ src/atoms/dropdown/DropDown.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+import { SelectWrap, BtnSelect, UlOption, BtnOption } from "./style";
+
+export interface DropDownProps {
+  options: string[];
+  width: number;
+  value?: string;
+  children?: React.ReactNode;
+}
+
+const DropDown = ({ options, width, value, children }: DropDownProps) => {
+  const [open, setOpen] = React.useState<boolean>(false);
+
+  const handleChange = (e: any) => {
+    console.log(e.target.value);
+  };
+  return (
+    <SelectWrap>
+      <h4 className="ir">{children}</h4>
+      <BtnSelect
+        className={`${open ? "open" : ""}`}
+        onClick={() => setOpen(!open)}
+        width={width}
+      >
+        {options[0]}
+      </BtnSelect>
+      {open === true ? (
+        <UlOption width={width}>
+          {options.map((option) => (
+            <li key={option}>
+              <BtnOption
+                key={option}
+                onClick={() => handleChange}
+                value={value}
+              >
+                {option}
+              </BtnOption>
+            </li>
+          ))}
+        </UlOption>
+      ) : null}
+    </SelectWrap>
+  );
+};
+
+export default DropDown;
