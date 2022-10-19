a1263ad2d0f909836863ba5d8ee979794a1dcbea
leeyeun <dpdms0109@naver.com>
2022-10-20 00:23:49 +0900

    :sparkles: Feat : 고양이 상세 페이지에 밥주기 모달 연결

--- src/pages/catInfoPage/CatInfoPage.tsx
+++ src/pages/catInfoPage/CatInfoPage.tsx
@@ -6,21 +6,30 @@ import Button from "../../atoms/button/Button";
 
 import CatInfo from "../../components/catInfo/CatInfo";
 import CatFeed from "../../components/catFeed/CatFeed";
+import CatFoodPageModal from "../../components/modal/catFoodPageModal/CatFoodPageModal";
 
 export const Wrap = styled.div`
   padding: 15px 40px 37px;
 `;
 
 const CatInfoPage = () => {
+  const [onModal, setModal] = React.useState<boolean>(false);
+  const OpenModal = () => {
+    setModal(true);
+  };
+  const CloseModal = () => {
+    setModal(false);
+  };
   return (
     <>
       <Header />
       <Wrap>
         <CatInfo />
         <CatFeed />
-        <Button marginTop={10} bgColor="var(--main-color)">
+        <Button marginTop={10} bgColor="var(--main-color)" onClick={OpenModal}>
           밥 주기
         </Button>
+        {onModal && <CatFoodPageModal CloseModal={CloseModal} />}
       </Wrap>
     </>
   );
