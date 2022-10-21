ae97552560936ba28ac05d21c4afdc94f5ae098d
Nam-Wijeong <dnlwjdv@hanmail.net>
2022-10-21 16:08:58 +0900

    :sparkles: Feat : 카카오맵 API 연결 및 홈 화면에 현위치 표시하기

---
+++ src/pages/recordMapPage/MyLocation.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react'
+import { Map, MapMarker } from 'react-kakao-maps-sdk'
+
+interface LocationType {
+  center: { lat: number, lng: number },
+  error?: { message: string };
+}
+
+const MyLocation = () => {
+  const [myLocation, setMyLocation] = useState<LocationType>({
+    center: { lat: 0, lng: 0 },
+  })
+
+  // 성공시 인자로 받은 위치의 경도, 위도 표시
+  const onSuccess = (location: { coords: { latitude: number; longitude: number; } }) => {
+    setMyLocation({
+      center: { lat: location.coords.latitude, lng: location.coords.longitude }
+    })
+  }
+  // 실패시
+  const onError = (error: { message: string; }) => {
+    setMyLocation({
+      center: { lat: 0, lng: 0 },
+      error,
+    })
+  }
+  useEffect(() => {
+    // 사용중인 브라우저 내에서 지리적 위치가 없는 경우 에러 메세지
+    if (!(navigator.geolocation)) {
+      onError({
+        message: "Geolocation is not supported.",
+      })
+    }
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+  }, [])
+  return (
+    <Map
+      center={myLocation.center}
+      style={{ width: "100%", height: "772px", zIndex: '0' }}
+    >
+      <MapMarker position={myLocation.center} />
+    </Map>
+  )
+}
+
+export default MyLocation
