0a90175da3106a5426687f88e7c42e7670408fab
Hyebin-Woo <dev.hyebin@gmail.com>
2022-10-24 18:12:22 +0900

    :lipstick: Style: 헤더 스타일 분리

--- src/components/header/style.ts
+++ src/components/header/style.ts
@@ -1,1 +1,21 @@
 import styled from "styled-components";
+
+export const HeaderWrap = styled.div`
+  height: 48px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  &::after {
+    content: "";
+    display: block;
+  }
+  img {
+    cursor: pointer;
+  }
+`;
+
+export const H2 = styled.h2`
+  font-size: 28px;
+  font-family: "Shinb7Regular";
+`;
