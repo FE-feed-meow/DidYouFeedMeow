587c252b7a7d5e63f3982f70f0dea97df142ea33
Hyebin-Woo <dev.hyebin@gmail.com>
2022-11-05 16:04:07 +0900

    :lipstick: Style: 밥주기 스크롤 구현

--- src/components/catFeed/CatFeed.tsx
+++ src/components/catFeed/CatFeed.tsx
@@ -8,11 +8,33 @@ export const FeedWrap = styled.section`
   display: block;
 `;
 
+export const FeedBox = styled.div`
+  padding: 5px;
+  width: 330px;
+  height: 170px;
+  overflow-y: auto;
+  overflow-x: hidden;
+  /* 스크롤바 설정 */
+  &::-webkit-scrollbar {
+    width: 9px;
+  }
+  /* 스크롤바 막대 설정 */
+  &::-webkit-scrollbar-thumb {
+    height: 17%;
+    background-color: #373737;
+    border-radius: 10px;
+  }
+  /* 스크롤바 뒷 배경 설정 */
+  &::-webkit-scrollbar-track {
+    background-color: #b5b5b5;
+  }
+`;
+
 export const DateTxt = styled.p`
-  font-size: 14px;
+  font-size: 16px;
+  font-weight: 500;
   color: #373737;
   padding-bottom: 30px;
-  width: 64px;
 `;
 
 interface Feed {
@@ -30,14 +52,16 @@ const CatFeed = ({ feedList }: FeedProps) => {
   return (
     <FeedWrap>
       <DateTxt> 22. 10. 04</DateTxt>
-      {feedList.length > 0
-        ? feedList.map((feed, i: number) => {
-            return <FeedCard key={feed.id} feed={feed} />;
-          })
-        : null}
+      <FeedBox>
+        {feedList.length > 0
+          ? feedList.map((feed, i: number) => {
+              return <FeedCard key={feed.id} feed={feed} />;
+            })
+          : null}
 
-      {/* <FeedCard />
+        {/* <FeedCard />
       <FeedCard /> */}
+      </FeedBox>
     </FeedWrap>
   );
 };
