ceb1384ea19d890e98b613b78d18e224a39da7d3
hee1231 <darong018@naver.com>
2022-11-14 22:16:03 +0900

    :sparkles: Feat : 스플래시 페이지 기능 구현

--- src/pages/splashPage/Splash.tsx
+++ src/pages/splashPage/Splash.tsx
@@ -1,5 +1,6 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router";
+import styled, { keyframes } from "styled-components";
 import Image from "../../atoms/image/Image";
 
 const Wrap = styled.div`
@@ -12,19 +13,47 @@ const Wrap = styled.div`
   }
 `;
 
+const ImageFadeOut = keyframes`
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+    visibility: hidden;
+  }
+`;
+
+const Animation = styled.div`
+  position: absolute;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  animation: 0.7s ease-out ${ImageFadeOut} forwards;
+  animation-delay: 0.7s;
+  z-index: 10;
+`;
+
 const Splash = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/login");
+    }, 1500);
+  });
   return (
-    <Wrap>
-      <Image
-        marginTop={300}
-        marginBottom={15}
-        width={113}
-        height={85}
-        alt=""
-        src="assets/images/logo.svg"
-      />
-      <p>밥줬냥</p>
-    </Wrap>
+    <Animation>
+      <Wrap>
+        <Image
+          marginTop={300}
+          marginBottom={15}
+          width={113}
+          height={85}
+          alt=""
+          src="assets/images/logo.svg"
+        />
+        <p>밥줬냥</p>
+      </Wrap>
+    </Animation>
   );
 };
 
