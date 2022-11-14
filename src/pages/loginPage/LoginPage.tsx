64df7860a45a3b4e9efd378adc0ed2ce508ef58a
ujin <dklvmf0106@gmail.com>
2022-11-14 19:50:02 +0900

    :sparkles: Feat: 로그인페이지, 회원가입페이지로 이동 기능 추가

--- src/pages/loginPage/LoginPage.tsx
+++ src/pages/loginPage/LoginPage.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import React from "react";
 
 import Button from "../../atoms/button/Button";
@@ -7,6 +8,11 @@ import { MiddleWrap } from "../../styles/commonStyle";
 import { H2, Join } from "./style";
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+  const navigateToJoin = () => {
+    navigate("/join");
+  };
+
   return (
     <MiddleWrap>
       <Image
@@ -17,12 +23,39 @@ const LoginPage = () => {
         src="assets/images/logo.svg"
       />
       <H2 style={{ marginBottom: 60 }}>로그인</H2>
-      <Inputs label="이메일" type="text" width={275} />
-      <Inputs label="비밀번호" type="text" width={275} />
-      <Button marginTop={12} bgColor="var(--disabled-button-color)">
+      <Inputs
+        label="이메일"
+        placeholder="이메일을 입력해주세요."
+        required
+        name="email"
+        type="email"
+        // value={email}
+        width={275}
+        // onChange={onChangeEmail}
+      />
+      <Inputs
+        label="비밀번호"
+        placeholder="비밀번호를 입력해주세요."
+        required
+        name="password"
+        type="password"
+        // value={password}
+        width={275}
+        // onChange={onChangePassword}
+      />
+      {/* {logInError && (
+        <ErrorMessage>* 이메일 또는 비밀번호가 일치하지 않습니다.</ErrorMessage>
+      )} */}
+      <Button
+        marginTop={12}
+        bgColor="var(--disabled-button-color)"
+        hoverBgColor="var(--main-color)"
+        type="submit"
+        // onClick={onLogin}
+      >
         입장하기
       </Button>
-      <Join>이메일로 회원가입</Join>
+      <Join onClick={navigateToJoin}>이메일로 회원가입</Join>
     </MiddleWrap>
   );
 };
