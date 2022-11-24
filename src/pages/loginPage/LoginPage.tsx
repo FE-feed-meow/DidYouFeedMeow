6898802b05523c8d088ba4856e2fbfa40276a272
ujin <dklvmf0106@gmail.com>
2022-11-25 01:50:06 +0900

    :lipstick: 로그인 UI 왼쪽 정렬로 변경

--- src/pages/loginPage/LoginPage.tsx
+++ src/pages/loginPage/LoginPage.tsx
@@ -58,38 +58,43 @@ const LoginPage = () => {
         src="assets/images/logo.svg"
       />
       <H2 style={{ marginBottom: 60 }}>로그인</H2>
-      <Inputs
-        label="이메일"
-        placeholder="이메일을 입력해주세요."
-        required
-        name="email"
-        type="email"
-        value={email}
-        width={275}
-        onChange={onChangeEmail}
-      />
-      <Inputs
-        label="비밀번호"
-        placeholder="비밀번호를 입력해주세요."
-        required
-        name="password"
-        type="password"
-        value={password}
-        width={275}
-        onChange={onChangePassword}
-      />
-      {logInError && (
-        <ErrorMessage>* 이메일 또는 비밀번호가 일치하지 않습니다.</ErrorMessage>
-      )}
-      <Button
-        marginTop={12}
-        bgColor="var(--disabled-button-color)"
-        hoverBgColor="var(--main-color)"
-        type="submit"
-        onClick={onLogin}
-      >
-        입장하기
-      </Button>
+      <div>
+        <Inputs
+          label="이메일"
+          placeholder="이메일을 입력해주세요."
+          required
+          name="email"
+          type="email"
+          value={email}
+          width={275}
+          onChange={onChangeEmail}
+        />
+        <Inputs
+          label="비밀번호"
+          placeholder="비밀번호를 입력해주세요."
+          required
+          name="password"
+          type="password"
+          value={password}
+          width={275}
+          onChange={onChangePassword}
+        />
+        {logInError && (
+          <ErrorMessage>
+            * 이메일 또는 비밀번호가 일치하지 않습니다.
+          </ErrorMessage>
+        )}
+        <Button
+          marginTop={12}
+          bgColor="var(--disabled-button-color)"
+          hoverBgColor="var(--main-color)"
+          type="submit"
+          onClick={onLogin}
+        >
+          입장하기
+        </Button>
+      </div>
+
       <Join onClick={navigateToJoinQuiz}>이메일로 회원가입</Join>
     </MiddleWrap>
   );
