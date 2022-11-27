1151e6fc8c8b2f19e7f18a7c15d2926c4207f8fc
Hyebin-Woo <dev.hyebin@gmail.com>
2022-11-27 21:25:05 +0900

    :sparkles: Add: 로딩 추가

--- src/pages/catListPage/CatListPage.tsx
+++ src/pages/catListPage/CatListPage.tsx
@@ -1,42 +1,58 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router';
-import axios from 'axios';
-import Header from '@components/header/Header';
-import { Catlist, CatName, CatAddress, CatBirth, CatImg, Title, CatListWrap } from './style';
+import React from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router";
+import axios from "axios";
+import Header from "@components/header/Header";
+import Loading from "../../components/loading/Loading";
+import {
+  Catlist,
+  CatName,
+  CatAddress,
+  CatBirth,
+  CatImg,
+  Title,
+  CatListWrap,
+} from "./style";
 
 const CatListPage = () => {
-  const token = localStorage.getItem('token')
+  const token = localStorage.getItem("token");
   const API_URL = "https://mandarin.api.weniv.co.kr";
-  const userInfo: any = localStorage.getItem('userInfo');
+  const userInfo: any = localStorage.getItem("userInfo");
   const [catListInfo, setCatListInfo] = React.useState("");
   const navigate = useNavigate();
+  const [loading, setLoading] = React.useState<boolean>(true);
 
   const getCatInfo = async () => {
     await axios
-      .get(`${API_URL}/post/${JSON.parse(userInfo).accountname}/userpost/?limit=20`,
+      .get(
+        `${API_URL}/post/${
+          JSON.parse(userInfo).accountname
+        }/userpost/?limit=20`,
         {
           headers: {
             Authorization: `Bearer ${token}`,
             "Content-Type": "application/json",
           },
-        })
+        },
+      )
       .then((response) => {
         const arrList = response.data.post.map((e: any) => {
-          const moveToDetailpage = () => { navigate(`/catInfo/${e.id}`) }
+          const moveToDetailpage = () => {
+            navigate(`/catInfo/${e.id}`);
+          };
           return (
             <Catlist key={e.id} onClick={moveToDetailpage}>
               <div>
-                <CatName>{(e.content.split('|')[0])}</CatName>
-                <CatBirth>출생년도 : {(e.content.split('|')[2])}</CatBirth>
-                <CatAddress>주소 : {(e.content.split('|')[1])}</CatAddress>
+                <CatName>{e.content.split("|")[0]}</CatName>
+                <CatBirth>출생년도 : {e.content.split("|")[2]}</CatBirth>
+                <CatAddress>주소 : {e.content.split("|")[1]}</CatAddress>
               </div>
               <CatImg src={e.image} alt="고양이 사진" />
             </Catlist>
-          )
-        }
-        );
+          );
+        });
         setCatListInfo(arrList);
+        setLoading(false);
       })
       .catch((error) => {
         console.log(error);
@@ -47,15 +63,24 @@ const CatListPage = () => {
   }, []);
 
   return (
-    <>
-      {!catListInfo && <div>로딩중!!!</div>}
-      <Header />
-      <Title>내가 등록한 냥이</Title>
-      <CatListWrap onScroll={() => { console.log('스크롤!!!') }}>
-        {catListInfo}
-      </CatListWrap>
-    </>
-  )
-}
+    <div>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <Header />
+          <Title>내가 등록한 냥이</Title>
+          <CatListWrap
+            onScroll={() => {
+              console.log("스크롤!!!");
+            }}
+          >
+            {catListInfo}
+          </CatListWrap>
+        </>
+      )}
+    </div>
+  );
+};
 
-export default CatListPage
+export default CatListPage;
